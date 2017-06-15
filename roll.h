8e1c71b23c8b4967c9c3045ebcd98bc1b996bc8a
Morihaus <villam1995@gmail.com>
2017-06-15 14:02:43 +0200

    Add files via upload

--- roll.h
+++ roll.h
@@ -8,19 +8,19 @@ class roll : public widgets
 {
 public:
     roll(int x,int y,int m_x,int m_y, std::vector<std::string> element);
-    virtual ~roll();
-    void close();
-    virtual void draw();
-    void rolling();
-    virtual void handle(genv::event);
-    virtual void choose(genv::event);
-    std::string things;
+    ~roll();
+    bool is_selected(int mouse_x, int mouse_y) const;
+    void draw();
+    void handle(genv::event);
+    // virtual void choose(genv::event);
     std::string get_things();
 
-protected:
+private:
     std::vector<std::string> elements;
     bool open=false;
-
+    void close();
+    void rolling();
+    std::string things;
 };
 
 #endif // roll_H
