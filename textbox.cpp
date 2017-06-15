996ef92d376507ca0b3c25031a1ae68e027d93ac
Morihaus <villam1995@gmail.com>
2017-06-15 17:46:26 +0200

    Add files via upload

--- textbox.cpp
+++ textbox.cpp
@@ -4,31 +4,31 @@
 using namespace std;
 using namespace genv;
 
-TextBox::TextBox(int x, int y, int m_x, int m_y, string s) : widgets(x,y,m_x,m_y)
+TextBox::TextBox(int x, int y, int m_x, int m_y,string s) : widgets(x,y,m_x,m_y)
 {
-    _s=s;
 }
+
+bool TextBox::is_selected( int mouse_x, int mouse_y ) const
+{
+    return mouse_x>_x && mouse_x<(_x+_m_x) && mouse_y>_y && mouse_y<(_y+_m_y);
+}
+
 void TextBox::draw()
 {
     gout << move_to(_x, _y) << color(179,255,254) << box(_m_x, _m_y);
     gout << color(0,0,0)<<move_to(_x+8, _y+20) << text(_s);
 }
+
 void TextBox::handle(event ev)
 {
-
-    if (ev.pos_x>_x && ev.pos_x<_x+_m_x && ev.pos_y>=_y && ev.pos_y<_y+_m_x)
+    gin.timer(500);
+    if (ev.type==ev_key && ev.keycode >= 48 && ev.keycode < 58 && _x+_s.length()*10<_x+_m_x-20)
     {
-        if (ev.keycode >= ' ' && ev.keycode < 255)
-        {
-            _s += ev.keycode;
-            if (_m_x < 10+gout.twidth(_s) )
-            {
-                _s = _s.substr(0,_s.length()-1);
-            }
-        }
-        if (ev.keycode == key_backspace)
-        {
-            _s = _s.substr(0,_s.length()-1);
-        }
+        _s+=char(ev.keycode);
     }
+    if (ev.type==ev_key && ev.keycode == key_backspace && _s.length()>=0)
+    {
+        _s.erase(_s.length()-1);
+    }
+
 }
--- textbox.hpp
+++ textbox.hpp
@@ -10,6 +10,7 @@ private:
     int ido1,ido2;
 public:
     TextBox(int x, int y, int m_x, int m_y, std::string s);
+    bool is_selected(int mouse_x, int mouse_y) const;
     void draw();
     void handle(genv::event);
 
